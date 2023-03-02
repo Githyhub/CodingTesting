f20045e4a6b7b4d8a5c3e5d4a12576b387ff16d7
Githyhub <102429804+Githyhub@users.noreply.github.com>
2023-03-02 14:54:51 -0700

    zsr

--- TestingFiles/BillFold/BillFold.java
+++ TestingFiles/BillFold/BillFold.java
@@ -42,4 +42,8 @@ public class BillFold {
         }
         return count;
     }
+
+    public String format(){
+
+    }
 }
--- TestingFiles/SavingAccount/WorkerTest.java
+++ TestingFiles/SavingAccount/WorkerTest.java
@@ -2,7 +2,7 @@ package SavingAccount;
 
 public class WorkerTest {
     public static void main(String[] args) {
-        Worker N = new Worker(15,"jameison");
+     
     }
 
 
